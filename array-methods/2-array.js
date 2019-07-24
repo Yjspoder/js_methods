0220c78c31ffe40593c1eb9acd7c54fc6fbe7bd6
yjspoder <injose.joshi@gmail.com>
2019-07-25 00:13:51 +0530

    add 2-array.js

--- array-methods/2-array.js
+++ array-methods/2-array.js
@@ -10,6 +10,14 @@ var words = [
 //Write a function findLongestWord that takes an array of words and returns the longest one.
 //If there are 2 with the same length, it should return the first occurrence.
 
+var num = 0;
+var longestString = "";
+words.forEach(function(element) {
+  if (element.length > num) {
+    num = element.length;
+    longestString = element;
+  }
+});
 
 
 
@@ -18,14 +26,23 @@ var numbers1 = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 // Use reduce method of array
 // Use the above sum and calculate the average.
 
-
+numbers1.reduce((acc , cv) =>{
+    acc = acc + cv;
+    return acc;
+}, 0)
 
 
 var numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 //Write a function averageNumbers that receives an array of numbers2 and calculate the average of the numbers
+function average(){
+	let avg = numbers2.reduce((acc , cv) => {
+        acc += cv;
+        return acc;
+})
+	return avg / numbers2.length;
+}
 
-
-
+average();
 
 var words2 = [
   'seat',
@@ -42,4 +59,12 @@ var words2 = [
 //Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
 
 
-
+var wordLength = 0;
+function avg(words2) {
+  words2.forEach(function(element) {
+    wordLength += element.length;
+  });
+  var wordAvg = wordLength / words2.length;
+  return wordAvg;
+}
+avg(words2);
